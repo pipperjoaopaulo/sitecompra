a8a82a05abf676038e76cfce02e9c9bf22704f77
pipperjoaopaulo <pipperjoaopaulo@gmail.com>
2026-09-25 10:59:47 -0300

    terminado

--- app/src/main/java/com/joaopaulo/myapplication/MainActivity.kt
+++ app/src/main/java/com/joaopaulo/myapplication/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.joaopaulo.myapplication
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.navigation.NavigationView
 import com.joaopaulo.myapplication.databinding.ActivityMainBinding
+import kotlin.text.replace
+import androidx.activity.addCallback
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var fragmentManager: FragmentManager
@@ -20,6 +26,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         WindowCompat.setDecorFitsSystemWindows(window,false)
         WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars())
 
@@ -28,7 +35,62 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val toggle = ActionBarDrawerToggle(this,binding.drawerLayout, binding.toolbar, R.string.nav_open,R.string.nav_close)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+
         binding.navigationDrawer.setNavigationItemSelectedListener(this)
 
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.bottom_home -> openFragment(HomeFragment())
+                R.id.botao_cart -> openFragment(CartFragment())
+                R.id.botao_profile -> openFragment(ProfileFragment())
+                R.id.botao_menu -> openFragment(MenuFragment())
+            }
+            true
         }
-    }
+        fragmentManager = supportFragmentManager
+        openFragment(HomeFragment())
+
+        binding.fab.setOnClickListener {
+            Toast.makeText(this, "Categorias" , Toast.LENGTH_SHORT).show()
+            }
+
+
+
+            onBackPressedDispatcher.addCallback(this) {
+               if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+                    binding.drawerLayout.closeDrawer(GravityCompat.START)
+               }else{
+                    finish()
+               }
+            }
+
+
+
+
+        }
+
+
+            override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+                when(p0.itemId){
+                    R.id.nav_pet -> openFragment(PetsFragment())
+                    R.id.nav_furniture -> openFragment(MobiliaFragment())
+                    R.id.nav_eletronics -> openFragment(EletrodomesticsFragment())
+                    R.id.nav_mobile -> Toast.makeText(this, "Telefones" , Toast.LENGTH_SHORT).show()
+                    R.id.nav_games ->  Toast.makeText(this, "Jogos" , Toast.LENGTH_SHORT).show()
+                    R.id.nav_cars -> Toast.makeText(this, "Carros" , Toast.LENGTH_SHORT).show()
+                }
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
+                return true
+            }
+
+
+
+            private fun openFragment(fragment: Fragment){
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.fragment_container,fragment)
+                fragmentTransaction.commit()
+            }
+}
+
+
+
